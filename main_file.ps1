c70ab623352eef5d28beefb4f581e66bc5e90362
TDB117 <lechartier@et.esiea.fr>
2023-02-06 11:44:12 +0100

    TASK5S

--- main_file.ps1
+++ main_file.ps1
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 Write-Host “Task4”
 
-=======
 Write-Host “Task5"
->>>>>>> ac09d22 (My commit)
-=======
-Write-Host “Task5"
->>>>>>> ac09d2295b8fde6ececb9aae9f65c20602f7a832
